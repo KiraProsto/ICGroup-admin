f6d8d695d585cbeae119a9ce9cc27eb615463196
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-03-10 13:02:09 +0300

    Add newsCounters component to NewsPage

--- src/components/news/NewsMockData.ts
+++ src/components/news/NewsMockData.ts
@@ -9,13 +9,15 @@ export interface INewItem {
   views: number;
 }
 
-export const mockNews: INewItem[] = Array.from({ length: 10 }).map((_, i) => ({
-  id: i + 1,
-  title: 'Пример длинного названия новости в несколько строк',
-  type: 'Тип публикации',
-  category: 'Название рубрики',
-  author: 'Иван Иванов',
-  date: '05.10.2021 в 12:38',
-  url: 'https://telesputnik.ru/materials/tech/news/1256',
-  views: 1256,
-}));
+export const mockNews: INewItem[] = Array.from({ length: 5723 }).map(
+  (_, i) => ({
+    id: i + 1,
+    title: 'Пример длинного названия новости в несколько строк',
+    type: 'Публичный',
+    category: 'Название рубрики',
+    author: 'Иван Иванов',
+    date: '05.10.2021 в 12:38',
+    url: 'https://telesputnik.ru/materials/tech/news/1256',
+    views: 1256,
+  }),
+);
--- src/pages/admin/NewsPage.tsx
+++ src/pages/admin/NewsPage.tsx
@@ -1,9 +1,17 @@
-import NewsHeader from "@/components/news/NewsHeader";
+import NewsCounters from '@/components/news/NewsCounters';
+import NewsHeader from '@/components/news/NewsHeader';
+import { mockNews } from '@/components/news/NewsMockData';
 
 export default function NewsPage() {
+  const all = mockNews.length;
+  const published = mockNews.filter((n) => n.type === 'Публичный').length;
+  const drafts = mockNews.filter((n) => n.type === 'Черновик').length;
+
   return (
     <div className="admin__content">
       <NewsHeader />
+
+      <NewsCounters all={all} published={published} drafts={drafts} />
     </div>
   );
 }
