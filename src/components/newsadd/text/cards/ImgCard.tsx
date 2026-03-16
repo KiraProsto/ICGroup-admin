c4584ebce4398e85dced39fa870618c2abf48631
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-03-16 14:41:39 +0300

    add image card

--- src/components/newsadd/text/cards/ImgCard.tsx
+++ src/components/newsadd/text/cards/ImgCard.tsx
@@ -1,3 +1,41 @@
+import { useState } from 'react';
+import Functions from './Functions';
+import './cards.css';
+
 export default function ImgCard() {
-  return <div></div>;
+  const [url, setUrl] = useState('');
+  const [alt, setAlt] = useState('');
+
+  return (
+    <div className="card">
+      <div className="card__header">
+        <h1 className="card__title">Изображение</h1>
+        <Functions />
+      </div>
+      <input
+        type="text"
+        className="card__input-title"
+        placeholder="URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <input
+        type="text"
+        className="card__input-title"
+        placeholder="Описание"
+        value={alt}
+        onChange={(e) => setAlt(e.target.value)}
+      />
+
+      {url && (
+        <div className="card__img-wrapper">
+          <img src={url} alt={alt || 'preview'} className="card__img-preview" />
+
+          <div className="card__img-overlay">
+            <span className="card__img-alt">{alt}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
