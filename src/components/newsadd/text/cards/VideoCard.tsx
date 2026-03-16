4cd8fc372df6dd8ba49634e960ef54e8ea74afcb
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-03-16 14:53:21 +0300

    add video card

--- src/components/newsadd/text/cards/VideoCard.tsx
+++ src/components/newsadd/text/cards/VideoCard.tsx
@@ -1,3 +1,33 @@
+import { useState } from 'react';
+import Functions from './Functions';
+import './cards.css';
+
 export default function VideoCard() {
-  return <div></div>;
+  const [url, setUrl] = useState('');
+  const [alt, setAlt] = useState('');
+
+  return (
+    <div className="card">
+      <div className="card__header">
+        <h1 className="card__title">Видео</h1>
+        <Functions />
+      </div>
+      <input
+        type="text"
+        className="card__input-title"
+        placeholder="URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <input
+        type="text"
+        className="card__input-title"
+        placeholder="Описание"
+        value={alt}
+        onChange={(e) => setAlt(e.target.value)}
+      />
+
+      {url && <video src={url} className="card__img-preview" controls />}
+    </div>
+  );
 }
